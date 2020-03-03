b19b87cba79663b417ba90de374024c6dfce2872
Troy Christian <apostasy8@gmail.com>
2020-03-03 14:43:52 -0500

    Create CategoryController

---
+++ SpendingSentinel/SpendingSentinel/CategoryController/CategoryController.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryController.swift
+//  SpendingSentinel
+//
+//  Created by Lambda_School_Loaner_219 on 3/3/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import Foundation
