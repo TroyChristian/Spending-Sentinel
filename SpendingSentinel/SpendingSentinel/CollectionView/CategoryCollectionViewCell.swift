51ecc154593ce54bca6c5f610cfa02cd31c73c9e
Troy Christian <apostasy8@gmail.com>
2020-03-03 12:14:41 -0500

    Add CollectionView Cell, set reuseIdentifier

---
+++ SpendingSentinel/SpendingSentinel/CollectionView/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  SpendingSentinel
+//
+//  Created by Lambda_School_Loaner_219 on 3/3/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
